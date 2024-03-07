41cab2c5c0440e7e4eaa6752cb16106f5a502d37
MD ANIKUL ISLAM <anikul.islam01@northsouth.edu>
2024-03-07 15:01:25 +0600

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>Log In | Boogie Lane Admin</title>
+    <title>Log In | Lemo xpress Admin</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully responsive admin theme which can be used to build CRM, CMS, ERP, etc." name="description" />
     <meta content="Your Name" name="author" />
